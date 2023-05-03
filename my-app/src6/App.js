fa2e1ed4e91a46ac8c76442a793c38e407f8d53b
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-05-03 15:08:40 +0100

    update flip coin game

---
+++ my-app/src6/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Flip from './Flip'
+import './App.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Flip />
+            </div>
+        )
+    }
+}
+
+export default App
+
+
