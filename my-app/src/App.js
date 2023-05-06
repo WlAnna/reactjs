5d8b89f4a3ee9345eac9392e69b893b26e7a4908
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-05-06 16:20:22 +0100

    remove yahtzee-starter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import ShoppingList from "./ShoppingList";
-import "./App.css";
+import React, { Component } from 'react';
+import GithubUserInfo from './GithubUserInfo';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ShoppingList />
+      <div className="App">
+        <GithubUserInfo username="WlAnna" />
       </div>
     );
   }
